aecdb1401b1efa96de38e922352c39b0f677077c
Gn.5 <Gn.5@LAPTOP-MHA1KKNG>
2021-11-28 23:26:46 +0200

    Add project files.

---
+++ AutoLotModel/Order.cs
@@ -0,0 +1,18 @@
+namespace AutoLotModel
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("Order")]
+    public partial class Order
+    {
+        public int OrderID { get; set; }
+
+        public int? CustID { get; set; }
+
+        public int? CarID { get; set; }
+    }
+}
